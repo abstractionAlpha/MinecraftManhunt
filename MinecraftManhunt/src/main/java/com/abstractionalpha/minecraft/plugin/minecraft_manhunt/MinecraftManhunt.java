83340c49f4ff1ead4bd78dbeb6e150494cb716b9
Mason <Mason@DESKTOP-NP6OR4H>
2021-10-16 21:23:36 -0400

    [Implementation] Added onEnable, onDisable methods

--- MinecraftManhunt/src/main/java/com/abstractionalpha/minecraft/plugin/minecraft_manhunt/MinecraftManhunt.java
+++ MinecraftManhunt/src/main/java/com/abstractionalpha/minecraft/plugin/minecraft_manhunt/MinecraftManhunt.java
@@ -3,5 +3,15 @@ package com.abstractionalpha.minecraft.plugin.minecraft_manhunt;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MinecraftManhunt extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// TODO insert startup logic
+	}
+	
+	@Override
+	public void onDisable() {
+		// TOD insert shutdown logic
+	}
 
 }
