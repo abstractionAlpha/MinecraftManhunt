310afb7807096204f9148256af6cbce8aae4da02
Mason <Mason@DESKTOP-NP6OR4H>
2021-10-16 21:36:03 -0400

    [Implementation] Added method headers for listener events

--- MinecraftManhunt/src/main/java/com/abstractionalpha/minecraft/plugin/minecraft_manhunt/MMListener.java
+++ MinecraftManhunt/src/main/java/com/abstractionalpha/minecraft/plugin/minecraft_manhunt/MMListener.java
@@ -1,7 +1,47 @@
 package com.abstractionalpha.minecraft.plugin.minecraft_manhunt;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class MMListener implements Listener {
+	
+	@EventHandler
+	public void onHunterInteract(PlayerInteractEvent pie) {
+		// TODO add interact code for hunter compass
+	}
+	
+	@EventHandler
+	public void onHunterDeath(PlayerDeathEvent pde) {
+		// TODO add hunter death code
+	}
+	
+	@EventHandler
+	public void onRunnerDeath(PlayerDeathEvent pde) {
+		// TODO add runner death code
+	}
+	
+	@EventHandler
+	public void onHunterJoin(PlayerJoinEvent pje) {
+		// TODO add hunter join code
+	}
+	
+	@EventHandler
+	public void onRunnerJoin(PlayerJoinEvent pje) {
+		// TODO add runner join code
+	}
+	
+	@EventHandler
+	public void onHunterQuit(PlayerQuitEvent pqe) {
+		// TODO add hunter quit code
+	}
+	
+	@EventHandler
+	public void onRunnerQuit(PlayerQuitEvent pqe) {
+		// TODO add runner quit code
+	}
 
 }
