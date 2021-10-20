85c99de9b6b263ffd100b25f929080bbfd1efec8
Mason <Mason@DESKTOP-NP6OR4H>
2021-10-20 11:38:06 -0400

    [Implementation] Implemented a class for the Runner object

---
+++ MinecraftManhunt/src/main/java/main/player/Runner.java
@@ -0,0 +1,40 @@
+package main.player;
+
+import java.util.HashMap;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class Runner {
+	
+	private Player p;
+	
+	private boolean alive;
+	
+	private HashMap<String, Location> locations;
+	
+	public Runner(Player p) {
+		this.p = p;
+		locations = new HashMap<String, Location>();
+		alive = true;
+		storeLocation();
+	}
+	
+	public void storeLocation() {
+		Location loc = p.getLocation();
+		locations.put(loc.getWorld().getName(), loc);
+	}
+	
+	public Location query(String worldName) {
+		return locations.get(worldName);
+	}
+	
+	public boolean isAlive() {
+		return alive;
+	}
+	
+	public void hasDied() {
+		alive = false;
+	}
+
+}
